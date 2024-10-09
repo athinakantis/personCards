c84147b976ae6e2f9503525df83980a39e43e766
[Athina Kantis] <[athina.kantis@gmail.com]>
2024-10-09 11:49:47 +0300

    first init

---
+++ main.js
@@ -0,0 +1,42 @@
+
+const cardsContainer = document.querySelector('.cards-container')
+
+
+async function fetchPerson() {
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json()
+        console.log(data)
+
+        data.forEach((person) => renderCard(person.name, person.username, person.email, person.id
+        ))
+
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+fetchPerson()
+
+
+function renderCard(name, username, email, id) {
+    cardsContainer.innerHTML +=
+        `<div class='card'>
+    <h3>${username}</h3>
+    <img src="https://robohash.org/${username}.png?size=200x200">
+    <p>${name}</p>
+    <p>${email}</p>
+    <p>${id}</p>
+    </div>`
+}
+
+
+
+
+/*
+10 cards
+user names
+robohash image
+name email username and ID
+upload to github pages 
+*/
